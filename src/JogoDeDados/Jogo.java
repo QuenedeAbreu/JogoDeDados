7bafebea275444bf4947d04f7651b550cc727376
Quenede Abreu <quenede.in@gmail.com>
2020-03-12 17:37:58 -0500

    Commit Teste

--- src/JogoDeDados/Jogo.java
+++ src/JogoDeDados/Jogo.java
@@ -1,5 +1,18 @@
 package JogoDeDados;
 
+import java.util.Scanner;
+
 public class Jogo {
-	
+	private static Scanner input;
+		public static void main(String[] args) {
+			String nomeJogador;
+			
+			input = new Scanner(System.in);
+			
+			System.out.println("Informe o nome do jogador: ");
+			nomeJogador = input.nextLine();
+			
+			
+			System.out.println(nomeJogador);
+		}
 }
